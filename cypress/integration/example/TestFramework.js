88a11fda6b13616ef8163ccf53e65eb9d40d83b1
neeraj.b.sharma <neeraj.b.sharma@rakute.com>
2023-06-01 21:54:59 +0530

    cypress automation

--- cypress/integration/example/TestFramework.js
+++ cypress/integration/example/TestFramework.js
@@ -14,6 +14,9 @@
     cy.get('select').select(this.data.gender)
     cy.get(':nth-child(4) > .ng-untouched').should('have.value',this.data.name)
     cy.get(':nth-child(2) > .nav-link').click()
-    cy.selectProduct('Blackberry')
+    this.data.productName.forEach(function(element){
+      cy.selectProduct(element)
+    });
+
    })
  })
