7bee053022394e4914ac0d63abc13f0bb15a97d1
neeraj.b.sharma <neeraj.b.sharma@rakute.com>
2023-06-01 21:58:27 +0530

    cypress automation

--- cypress/integration/example/TestFramework.js
+++ cypress/integration/example/TestFramework.js
@@ -13,6 +13,7 @@
     cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
     cy.get('select').select(this.data.gender)
     cy.get(':nth-child(4) > .ng-untouched').should('have.value',this.data.name)
+    cy.pause()
     cy.get(':nth-child(2) > .nav-link').click()
     this.data.productName.forEach(function(element){
       cy.selectProduct(element)
